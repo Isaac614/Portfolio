92f0e9faaad20766156717241a8bece4c1284530
Isaac614 <moo24006@byui.edu>
2025-07-09 20:08:43 -0600

    tilt effect has been added to project cards

--- js/projects.js
+++ js/projects.js
@@ -1,17 +1,30 @@
 import { projectInfo } from "./components/projectInfo.js";
 
 initialize();
-// openModal(new projectInfo("Title", "lorem ipsum", "images/sudoku-solver.png", "#", "#", "#", true));
+
+// const projectContainers = document.querySelectorAll('.projectContainer');
+// projectContainers.forEach(container => {
+//     container.addEventListener("mousemove", (event) => {
+//         if (event.currentTarget.className === "projectContainer") {
+//             cardTilt(event);
+//         }
+//        
+//     });
+//     container.addEventListener("mouseleave", (event) => {
+//         if (event.currentTarget.className === "projectContainer") {
+//             const project = container.querySelector('.project');
+//             project.style.transform = `rotateX(0deg)
+//             rotateY(0deg)
+//             translateX(0px)
+//             translateY(0px)
+//             translateZ(0px)`
+//         }
+//     })
+// });
+
 
 function initialize() {
-    // Get container element
     const container = document.querySelector(".projects");
-    
-    // for (let i = 0; i < 3; i++) {
-    //     const project = new projectInfo("Reminders", "lorem ipsum", "https://placehold.co/400", "none", "N/A");
-    // }
-    
-    
     
     fetch("js/data/projects.json")
         .then (response => {
@@ -33,25 +46,45 @@ function initialize() {
                 container.appendChild(projectCard);
                 left = !left;
             })
+            const projectContainers = document.querySelectorAll(".projectContainer");
+            projectContainers.forEach(container => {
+                container.addEventListener("mousemove", (event) => {
+                    if (event.currentTarget.className === "projectContainer") {
+                        cardTilt(event);
+                    }
+
+                });
+                container.addEventListener("mouseleave", (event) => {
+                    if (event.currentTarget.className === "projectContainer") {
+                        const project = container.querySelector('.project');
+                        project.style.transform = `rotateX(0deg)
+            rotateY(0deg)
+            translateX(0px)
+            translateY(0px)
+            translateZ(0px)`
+                    }
+                })
+            });
         })
         .catch(error => console.log(error));
 }
 
 
-function CreateProjectCard(projectInfo, left)
-{
+function CreateProjectCard(projectInfo, left) {
     const tempDiv = document.createElement("div");
-    tempDiv.innerHTML = 
-        `<div>
-            <img class="project-img">
-            <h1 class="project-name"></h1>
-            <p class="project-description"></p>
-            <a class="button" target="_blank">GitHub</a>
-            <a class="button" target="_blank">Demo</a>
+    tempDiv.innerHTML =
+        `
+        <div class="projectContainer">
+            <div class="project">
+                <img class="project-img">
+                <h1 class="project-name"></h1>
+                <p class="project-description"></p>
+                <a class="button" target="_blank">GitHub</a>
+                <a class="button" target="_blank">Demo</a>
+            </div>
         </div>`
-    const projectCard = tempDiv.querySelector("div");
-    
-    projectCard.classList.add(left ? "project-box-left" : "project-box-right");
+    const projectCard = tempDiv.querySelector(".project");
+    projectCard.classList.add(left ? "left" : "right");
     projectCard.querySelector('img').src = projectInfo.imageUrl;
     projectCard.querySelector('img').alt = `Image of ${projectInfo.title}`;
     projectCard.querySelector("h1").innerText = projectInfo.title;
@@ -65,25 +98,28 @@ function CreateProjectCard(projectInfo, left)
         projectCard.removeChild(buttons[1]);
     }
     projectCard.addEventListener("click", () => {
-        createModal(projectInfo);//new projectInfo("Title", "lorem ipsum", "images/sudoku-solver.png", "#", "#", "#", true));
+        createModal(projectInfo);
     });
-    return projectCard;
+    return tempDiv.querySelector(".projectContainer");
 }
 
 function createModal(projectInfo) {
     const left = true;
     const template = document.createElement("dialog");
     template.innerHTML =
-        `<div>
-            <img class="project-img">
-            <h1 class="project-name"></h1>
-            <p class="project-description"></p>
-            <a class="button" target="_blank">GitHub</a>
-            <a class="button" target="_blank">Demo</a>
+        `
+        <div class="projectContainer">
+            <div class="project">
+                <img class="project-img">
+                <h1 class="project-name"></h1>
+                <p class="project-description"></p>
+                <a class="button" target="_blank">GitHub</a>
+                <a class="button" target="_blank">Demo</a>
+            </div>
         </div>`
-    const projectCard = template.querySelector("div");
-
-    projectCard.classList.add(left ? "project-box-left" : "project-box-right");
+    const projectCard = template.querySelector(".project");
+    
+    projectCard.classList.add(left ? "leftCard" : "rightCard");
     projectCard.querySelector('img').src = projectInfo.imageUrl;
     projectCard.querySelector('img').alt = `Image of ${projectInfo.title}`;
     projectCard.querySelector("h1").innerText = projectInfo.title;
@@ -96,8 +132,48 @@ function createModal(projectInfo) {
     if (!projectInfo.hasDemo) {
         projectCard.removeChild(buttons[1]);
     }
-    
-    
-    document.querySelector("main").appendChild(template);
+    document.querySelector(".projects").appendChild(template);
     template.showModal();
+}
+
+function cardTilt(event) {
+    const card = event.currentTarget.querySelector(".project");
+    const container = event.currentTarget;
+
+    // Get container bounds
+    const rect = container.getBoundingClientRect();
+
+    // Calculate mouse position relative to container
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    const cardWidth = container.offsetWidth;
+    const cardHeight = container.offsetHeight;
+
+    const cardCenterX = cardWidth / 2;
+    const cardCenterY = cardHeight / 2;
+
+    // Much more subtle rotation range - like in the video
+    const rotationRange = 4;
+    const percentX = (mouseX - cardCenterX) / cardCenterX;
+    const percentY = (mouseY - cardCenterY) / cardCenterY;
+
+    // Clamp values to prevent over-rotation
+    const clampedPercentX = Math.max(-1, Math.min(1, percentX));
+    const clampedPercentY = Math.max(-1, Math.min(1, percentY));
+
+    const rotationX = -clampedPercentY * rotationRange;
+    const rotationY = clampedPercentX * rotationRange;
+
+    // Add subtle translation for more dynamic feel
+    const translateX = clampedPercentX * 2;
+    const translateY = clampedPercentY * 2;
+
+    card.style.transform = `
+        rotateX(${rotationX}deg) 
+        rotateY(${rotationY}deg)
+        translateX(${translateX}px)
+        translateY(${translateY}px)
+        translateZ(10px)
+    `;
 }
